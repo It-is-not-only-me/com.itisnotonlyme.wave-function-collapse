5630c1e837511f5b85ef1de7c13e24cd1631e82f
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2022-06-15 14:41:51 -0300

    Agregando muchas pruebas y el algoritmo

---
+++ Tests/Editor/GeneradorInicioPrueba.cs
@@ -0,0 +1,9 @@
+﻿using ItIsNotOnlyMe.WaveFunctionCollapse;
+
+public class GeneradorInicioPrueba : IGeneradorDeNumeros
+{
+    public int Numero(int final, int inicio = 0, int separacion = 1)
+    {
+        return inicio;
+    }
+}
--- Tests/Editor/WaveFunctionCollapseTest.cs
+++ Tests/Editor/WaveFunctionCollapseTest.cs
@@ -5,50 +5,35 @@ using UnityEngine;
 using UnityEngine.TestTools;
 using ItIsNotOnlyMe.WaveFunctionCollapse;
 
-public class EstadoPrueba : IEstado
-{
-    public int Numero { get; }
-
-    public EstadoPrueba(int numero)
-    {
-        Numero = numero;
-    }
-
-    public bool SeElimina(List<IEstado> estados, IValor valor)
-    {
-        bool seElimina = estados.Count > 0;
-        foreach (IEstado estado in estados)
-        {
-            EstadoPrueba estadoPrueba = estado as EstadoPrueba;
-            seElimina &= Numero == estadoPrueba.Numero;
-        }
-        return seElimina;
-    }
-}
-
-public class GeneradorInicioPrueba : IGeneradorDeNumeros
-{
-    public int Numero(int final, int inicio = 0, int separacion = 1)
-    {
-        return inicio;
-    }
-}
-
-public class ValorPruba : IValor
-{
-
-}
-
 public class WaveFunctionCollapseTest
 {
     private IGeneradorDeNumeros _generadorDeNumeros = new GeneradorInicioPrueba();
-
-    private IEstado _estado1 = new EstadoPrueba(1);
-    private IEstado _estado2 = new EstadoPrueba(2);
-    private IEstado _estado3 = new EstadoPrueba(3);
-    private IEstado _estado4 = new EstadoPrueba(4);
-
     private IValor _valor = new ValorPruba();
+
+    private IEstado _estado1, _estado2, _estado3, _estado4, _estado5, _estado6, _estado7, _estado8, _estado9;
+
+    public WaveFunctionCollapseTest()
+    {
+        _estado1 = new EstadoPrueba(1);
+        _estado2 = new EstadoPrueba(2);
+        _estado3 = new EstadoPrueba(3);
+        _estado4 = new EstadoPrueba(4);
+        _estado5 = new EstadoPrueba(5);
+        _estado6 = new EstadoPrueba(6);
+        _estado7 = new EstadoPrueba(7);
+        _estado8 = new EstadoPrueba(8);
+        _estado9 = new EstadoPrueba(9);
+    }
+
+    private List<IEstado> CuatroEstados()
+    {
+        return new List<IEstado> { _estado1, _estado2, _estado3, _estado4 };
+    }
+
+    private List<IEstado> NueveEstados()
+    {
+        return new List<IEstado> { _estado1, _estado2, _estado3, _estado4, _estado5, _estado6, _estado7, _estado8, _estado9 };
+    }
 
     [Test]
     public void Test01LaEntropiaDeUnNodoSinEstadosEsCero()
@@ -159,5 +144,91 @@ public class WaveFunctionCollapseTest
         Assert.AreEqual(1, principal.Entropia());
         Assert.AreEqual(1, secundario.Entropia());
         Assert.AreEqual(1, terciario.Entropia());
+    }
+
+    [Test] 
+    public void Test07ConTresNodosEnTrianguloAlUsarElAlgoritmoTodasLasEntropiasSonUno()
+    {
+        INodo principal = new Nodo(new List<IEstado> { _estado1, _estado2, _estado3 });
+        INodo secundario = new Nodo(new List<IEstado> { _estado1, _estado2, _estado3 });
+        INodo terciario = new Nodo(new List<IEstado> { _estado1, _estado2, _estado3 });
+
+        IArista aristaAPri = new Arista(principal, _valor);
+        IArista aristaASec = new Arista(secundario, _valor);
+        IArista aristaATri = new Arista(terciario, _valor);
+
+        principal.AgregarAdyacente(aristaASec);
+        principal.AgregarAdyacente(aristaATri);
+
+        secundario.AgregarAdyacente(aristaAPri);
+        secundario.AgregarAdyacente(aristaATri);
+
+        terciario.AgregarAdyacente(aristaAPri);
+        terciario.AgregarAdyacente(aristaASec);
+
+        List<INodo> nodos = new List<INodo> { principal, secundario, terciario };
+
+        WaveFunctionCollapse.Ejecutar(ref nodos, _generadorDeNumeros);
+
+        Assert.AreEqual(1, principal.Entropia());
+        Assert.AreEqual(1, secundario.Entropia());
+        Assert.AreEqual(1, terciario.Entropia());
+
+        EstadoPrueba estadoPrincipal = principal.EstadoFinal() as EstadoPrueba;
+        EstadoPrueba estadoSecundario = secundario.EstadoFinal() as EstadoPrueba;
+        EstadoPrueba estadoTerciario = terciario.EstadoFinal() as EstadoPrueba;
+
+        Assert.AreEqual(1, estadoPrincipal.Numero);
+        Assert.AreEqual(2, estadoSecundario.Numero);
+        Assert.AreEqual(3, estadoTerciario.Numero);
+    }
+
+    [Test]
+    public void Test08ConCuatrosNodosTodosVinculadosConTodosAlUsarElAlgoritmoTodasLasEntropiasSonUno()
+    {
+        List<INodo> nodos = new List<INodo>();
+
+        for (int i = 0; i < 4; i++)
+            nodos.Add(new Nodo(CuatroEstados()));
+
+        for (int i = 0; i < nodos.Count; i++)
+            for (int j = 0; j < nodos.Count; j++)
+            {
+                if (i == j)
+                    continue;
+                nodos[i].AgregarAdyacente(new Arista(nodos[j], _valor));
+            }
+
+        WaveFunctionCollapse.Ejecutar(ref nodos, _generadorDeNumeros);
+
+        foreach (INodo nodo in nodos)
+            Assert.AreEqual(1, nodo.Entropia());
+    }
+
+    [Test]
+    public void Test09ConNueveNodosTodosVinculadosConTodosAlUsarElAlgoritmoTodosLasEntropiasSonUno()
+    {
+        List<INodo> nodos = new List<INodo>();
+
+        for (int i = 0; i < 9; i++)
+            nodos.Add(new Nodo(NueveEstados()));
+
+        for (int i = 0; i < nodos.Count; i++)
+            for (int j = 0; j < nodos.Count; j++)
+            {
+                if (i == j)
+                    continue;
+                nodos[i].AgregarAdyacente(new Arista(nodos[j], _valor));
+            }
+
+        WaveFunctionCollapse.Ejecutar(ref nodos, _generadorDeNumeros);
+
+        int contador = 1;
+        foreach (INodo nodo in nodos)
+        {
+            Assert.AreEqual(1, nodo.Entropia());
+            EstadoPrueba estadoNodo = nodo.EstadoFinal() as EstadoPrueba;
+            Assert.AreEqual(contador++, estadoNodo.Numero);
+        }
     }
 }
